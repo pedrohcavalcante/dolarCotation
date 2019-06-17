ef442d72ef2e9ccffe1baafe69f8906307a66c41
Pedro Henrique Bezerra Cavalcante <pedrohbcavalcante@outlook.com>
2019-06-17 16:43:51 -0300

    commit inicial
    Signed-off-by: Pedro Henrique Bezerra Cavalcante <pedrohbcavalcante@outlook.com>

---
+++ src/main/java/br/ufrn/imd/DolarCotation/DolarCotationApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.DolarCotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolarCotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolarCotationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufrn/imd/DolarCotation/DolarCotationApplicationTests.java
@@ -0,0 +1,16 @@
+package br.ufrn.imd.DolarCotation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DolarCotationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
